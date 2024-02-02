67a256bca3f20759a2d19eaa81c20bf3fb9cc80c
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-02-02 14:46:07 +0500

    s

---
+++ Employees Management/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employees_Management.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
